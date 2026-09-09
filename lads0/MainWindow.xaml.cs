d64bcf7e210d3cdc2aff0162641a51ed8477c435
Alisonlima <aurin7128@gmail.com>
2026-09-09 21:30:04 +0700

    треугольник со случайными координатами

--- lads0/MainWindow.xaml.cs
+++ lads0/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab0;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,45 @@ namespace lads0
     /// </summary>
     public partial class MainWindow : Window
     {
+        Triangle tr;
+        Random rnd = new Random();
         public MainWindow()
         {
             InitializeComponent();
+            CreateRandomTriangle();
+            DrawTriangle(tr!);
+        }
+
+        private void CreateRandomTriangle()
+        {
+            Point2D p1 = new Point2D(rnd.Next(50, (int)Scene.Width - 50), rnd.Next(50, (int)Scene.Height - 50));
+            Point2D p2 = new Point2D(rnd.Next(50, (int)Scene.Width - 50), rnd.Next(50, (int)Scene.Height - 50));
+            Point2D p3 = new Point2D(rnd.Next(50, (int)Scene.Width - 50), rnd.Next(50, (int)Scene.Height - 50));
+            tr = new Triangle(p1, p2, p3);
+        }
+
+        public void DrawLine(Point2D p1, Point2D p2)
+        {
+            Line line = new Line();
+            line.Stroke = Brushes.Red;
+            line.StrokeThickness = 3;
+            line.X1 = p1.X;
+            line.Y1 = p1.Y;
+            line.X2 = p2.X;
+            line.Y2 = p2.Y;
+            Scene.Children.Add(line);
+        }
+
+        public void DrawTriangle(Triangle tr)
+        {
+            DrawLine(tr.P1, tr.P2);
+            DrawLine(tr.P2, tr.P3);
+            DrawLine(tr.P3, tr.P1);
+        }
+
+        public void ClearScene()
+        {
+            Scene.Children.Clear();
         }
     }
 }
